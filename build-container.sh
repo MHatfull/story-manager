1cd42b5dbfaa56469a58b5e0e7641af0049c4b19
Max Hatfull <max.hatfull@btinternet.com>
2018-08-12 19:58:50 +0100

    tag and upload container

--- build-container.sh
+++ build-container.sh
@@ -1,1 +1,2 @@
 docker build -t underlunchers/story-manager .
+docker tag underlunchers/story-manager:latest 167591810112.dkr.ecr.us-east-1.amazonaws.com/underlunchers/story-manager:latest
---
+++ upload-container.sh
@@ -0,0 +1,1 @@
+docker push 167591810112.dkr.ecr.us-east-1.amazonaws.com/underlunchers/story-manager:latest
